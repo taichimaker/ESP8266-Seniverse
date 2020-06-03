7a912914f5827f4e4cdfd7d0823dc98aba7a1b20
Cyno-Shuo <3106244@qq.com>
2020-06-03 23:50:19 +0800

    remove unecessary codes

--- src/WeatherNow.h
+++ src/WeatherNow.h
@@ -38,36 +38,4 @@ class WeatherNow {
     String _last_update_str = "no_init";   
     
 };
-
-// 获取天气预报信息类
-class WeatherForecast{
-  public:
-    WeatherForecast();
-
-    void config(String userKey, String location, String unit);
-    bool update();
-    String getWeatherText();
-    int getWeatherCode();
-    int getDegree();
-    String getLastUpdate();
-    String getServerCode();
-    
-  private:  
-    const char* _host = "api.seniverse.com";     // 服务器地址  
-        
-    String _reqUserKey;   // 私钥
-    String _reqLocation;  // 城市
-    String _reqUnit;      // 摄氏/华氏
-
-    void _parseNowInfo(WiFiClient client);      // 解析实时天气信息信息
-    
-    String _status_response =  "no_init"; // 服务器响应状态行
-    String _response_code =  "no_init";   // 服务器响应状态码
-    
-    String _now_text_str = "no_init";    
-    int _now_code_int = 404; 
-    int _now_temperature_int = 404;     
-    String _last_update_str = "no_init";   
-    
-}; 
 #endif
